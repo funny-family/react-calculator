c65c55cc198a9355a5785bf09f75753ce433a264
funny family <maksvadik228@gmail.com>
2020-10-25 01:20:41 +0700

    set up project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jsx-a11y'
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx']
+      }
+    ],
+    'react/react-in-jsx-scope': 'off'
+  }
+};

e8f61e3d21c014caa74cc88222d3e406ccb3b0ae
funny family <maksvadik228@gmail.com>
2020-10-26 11:10:01 +0700

    refactor(components): update Container component
    Update component layout and added calculation logic.

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,21 +1,186 @@
-import Input from '../Input';
+import { useState } from 'react';
+
+// import Input from '../Input';
+// https://www.youtube.com/watch?v=wOENJWPu23U&ab_channel=JustinKim
 import Button from '../Button';
 
 import './style.css';
 
 function Container() {
+  const [calculationValue, setCalculationValue] = useState('0');
+  const [stack, setStack] = useState(null);
+  const [operator, setOperator] = useState(null);
+
+  // const [inputValue, setInputValue] = useState('');
+
+  const handleButtonEvent = (calctype) => () => {
+    // console.log('calctype:', calctype);
+    if (calctype === 'AC') {
+      setCalculationValue('0');
+      setStack(null);
+      setOperator(null);
+      return;
+    }
+
+    if (calctype === '.') {
+      if (calculationValue.includes('.')) return;
+
+      setCalculationValue(`${calculationValue}.`);
+      return;
+    }
+
+    if (calctype === '+') {
+      if (operator !== null) {
+        if (operator === '+') {
+          setStack(stack + parseFloat(calculationValue));
+        } else if (operator === '-') {
+          setStack(stack - parseFloat(calculationValue));
+        } else if (operator === '*') {
+          setStack(stack * parseFloat(calculationValue));
+        } else if (operator === '/') {
+          setStack(stack / parseFloat(calculationValue));
+        }
+      } else {
+        setStack(parseFloat(calculationValue));
+      }
+
+      setCalculationValue('0');
+      setOperator('+');
+      return;
+    }
+
+    if (calctype === '-') {
+      if (operator !== null) {
+        if (operator === '+') {
+          setStack(stack + parseFloat(calculationValue));
+        } else if (operator === '-') {
+          setStack(stack - parseFloat(calculationValue));
+        } else if (operator === '*') {
+          setStack(stack * parseFloat(calculationValue));
+        } else if (operator === '/') {
+          setStack(stack / parseFloat(calculationValue));
+        }
+      } else {
+        setStack(parseFloat(calculationValue));
+      }
+
+      setCalculationValue('0');
+      setOperator('-');
+      return;
+    }
+
+    // if (calctype === '−') {
+    //   if (operator !== null) {
+    //     if (operator === '+') {
+    //       setStack(stack + parseFloat(calculationValue));
+    //     } else if (operator === '−') {
+    //       setStack(stack - parseFloat(calculationValue));
+    //     } else if (operator === '*') {
+    //       setStack(stack * parseFloat(calculationValue));
+    //     } else if (operator === '/') {
+    //       setStack(stack / parseFloat(calculationValue));
+    //     }
+    //   } else {
+    //     setStack(parseFloat(calculationValue));
+    //   }
+    //   setCalculationValue('0');
+    //   setOperator('−');
+    //   return;
+    // }
+
+    if (calctype === '*') {
+      if (operator !== null) {
+        if (operator === '+') {
+          setStack(stack + parseFloat(calculationValue));
+        } else if (operator === '-') {
+          setStack(stack - parseFloat(calculationValue));
+        } else if (operator === '*') {
+          setStack(stack * parseFloat(calculationValue));
+        } else if (operator === '/') {
+          setStack(stack / parseFloat(calculationValue));
+        }
+      } else {
+        setStack(parseFloat(calculationValue));
+      }
+
+      setCalculationValue('0');
+      setOperator('*');
+      return;
+    }
+
+    if (calctype === '/') {
+      if (operator !== null) {
+        if (operator === '+') {
+          setStack(stack + parseFloat(calculationValue));
+        } else if (operator === '-') {
+          setStack(stack - parseFloat(calculationValue));
+        } else if (operator === '*') {
+          setStack(stack * parseFloat(calculationValue));
+        } else if (operator === '/') {
+          setStack(stack / parseFloat(calculationValue));
+        }
+      } else {
+        setStack(parseFloat(calculationValue));
+      }
+
+      setCalculationValue('0');
+      setOperator('/');
+      return;
+    }
+
+    if (calctype === '=') {
+      if (!operator) return;
+
+      // console.log('calctype:', calctype);
+      // console.log('calc operation:', operator);
+
+      if (operator === '+') {
+        console.log('operator:', operator);
+        console.log('stack:', stack);
+        console.log('calculationValue:', parseFloat(calculationValue));
+        setCalculationValue((stack + parseFloat(calculationValue)).toString());
+      } else if (operator === '−') {
+        console.log('operator:', operator);
+        console.log('i am "-"'); // don't show in console!
+        console.log('stack:', stack);
+        console.log('calculationValue:', calculationValue);
+        setCalculationValue((stack - parseFloat(calculationValue)).toString());
+      } else if (operator === '*') {
+        setCalculationValue((stack * parseFloat(calculationValue)).toString());
+      } else if (operator === '/') {
+        setCalculationValue((stack / parseFloat(calculationValue)).toString());
+      }
+
+      setStack(null);
+      setOperator(null);
+      return;
+    }
+
+    if (calculationValue[calculationValue.length - 1] === '.') {
+      setCalculationValue(calculationValue + calctype);
+    } else {
+      // const number = parseFloat(calculationValue);
+      setCalculationValue(parseFloat(parseFloat(calculationValue) + calctype).toString());
+    }
+  };
+
   return (
     <div className="container-lg calc-container">
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <div className="input-group input-group-lg">
+            <h2 className="float-right">{ calculationValue }</h2>
+            {/* <div className="input-group input-group-lg">
               <Input
+                className="form-control"
                 type="text"
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
                 aria-label="Sizing example input"
                 aria-describedby="inputGroup-sizing-lg"
+                readOnly
               />
-            </div>
+            </div> */}
           </div>
         </div>
 
@@ -25,131 +190,160 @@ function Container() {
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                7
-              </Button>
+                calctype="&nbsp;"
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                8
-              </Button>
+                calctype="&nbsp;"
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                9
-              </Button>
+                calctype="&nbsp;"
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                &times;
-              </Button>
-            </div>
-            <div className="w-100" />
-            <div className="col-3 py-1">
-              <Button
-                className="btn btn-light btn-lg btn-block"
-                type="button"
-              >
-                4
-              </Button>
+                calctype="AC"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                5
-              </Button>
+                calctype="7"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                6
-              </Button>
+                calctype="8"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                ÷
-              </Button>
+                calctype="9"
+                onClick={handleButtonEvent}
+              />
+            </div>
+            <div className="col-3 py-1">
+              <Button
+                className="btn btn-light btn-lg btn-block"
+                type="button"
+                calctype="*"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="w-100" />
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                1
-              </Button>
+                calctype="4"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                2
-              </Button>
+                calctype="5"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                3
-              </Button>
+                calctype="6"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                -
-              </Button>
+                calctype="/"
+                onClick={handleButtonEvent}
+              />
+            </div>
+            <div className="w-100" />
+            <div className="col-3 py-1">
+              <Button
+                className="btn btn-light btn-lg btn-block"
+                type="button"
+                calctype="1"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                0
-              </Button>
+                calctype="2"
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                .
-              </Button>
+                calctype="3"
+                onClick={handleButtonEvent}
+              />
+            </div>
+            <div className="col-3 py-1">
+              <Button
+                className="btn btn-light btn-lg btn-block"
+                type="button"
+                calctype="-"
+                onClick={handleButtonEvent}
+              />
+            </div>
+            <div className="col-3 py-1">
+              <Button
+                className="btn btn-light btn-lg btn-block"
+                type="button"
+                calctype="0"
+                onClick={handleButtonEvent}
+              />
+            </div>
+            <div className="col-3 py-1">
+              <Button
+                className="btn btn-light btn-lg btn-block"
+                type="button"
+                calctype="."
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-primary btn-lg btn-block"
                 type="button"
-              >
-                =
-              </Button>
+                calctype="="
+                onClick={handleButtonEvent}
+              />
             </div>
             <div className="col-3 py-1">
               <Button
                 className="btn btn-light btn-lg btn-block"
                 type="button"
-              >
-                +
-              </Button>
+                calctype="+"
+                onClick={handleButtonEvent}
+              />
             </div>
           </div>
         </div>
